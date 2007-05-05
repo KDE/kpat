5cc372e727452043a5fe8aa2a71e8f8ac930d053
Robert Knight <robertknight@gmail.com>
2007-05-05 15:48:07 +0000

    Remove frame around edge of playing field.
    svn path=/trunk/KDE/kdegames/kpat/; revision=661453

--- view.cpp
+++ view.cpp
@@ -74,7 +74,7 @@ PatienceView::PatienceView( KXmlGuiWindow* _parent )
 {
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
+    setFrameStyle(QFrame::NoFrame);
     setAlignment( Qt::AlignLeft | Qt::AlignTop );
 
     assert(!s_instance);
