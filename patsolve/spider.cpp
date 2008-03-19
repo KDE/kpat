b0f76d9a4364829d1c4ed7affe9ccf1b933b87c5
Stephan Kulow <coolo@kde.org>
2008-03-19 20:54:42 +0000

    found one case where this is not a good idea
    svn path=/trunk/KDE/kdegames/kpat/; revision=787760

--- patsolve/spider.cpp
+++ patsolve/spider.cpp
@@ -307,7 +307,7 @@ int SpiderSolver::get_possible_moves(int *a, int *numout)
                      RANK(card) == RANK(*Wp[j]) - 1 )
                 {
                     allowed = true;
-                    if ( ( SUIT( card ) != SUIT( *Wp[j] ) ) && ( foundgood && Wlen[i] > l + 1 )) {
+                    if ( ( SUIT( card ) != SUIT( *Wp[j] ) ) && ( foundgood && Wlen[i] > l + 1 && l > 0 )) {
                         allowed = false; // make the tree simpler
                     }
                 }
