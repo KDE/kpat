aefb21c689812cd316b1f11d924d68f0b2073dbf
Parker Coates <coates@kde.org>
2009-09-01 15:59:21 +0000

    The Big KPat Licensing Update Commit!
    - A single project wide COPYING file explains current licensing.
    - All source files should now have a license header.
    - All source files should have at least one copyright line.
    - The entire patsolve directory is now GPLv2+.
    - All "newer" source files are now GPLv2+.
    - Files that formerly had Paul's original custom license are now licensed such that all changes made after 2009-01-01 will be GPLv2+.
    - Effort has been made to include copyright lines from original authors and accurate dates where possible. (I'm sure there's still a lot that could be done here, but one can only glean so much from CVS/SVN logs.)
    
    Coolo, I realise this is about as exciting as watching paint dry, but could you skim through the diff and look for any blaring mistakes? I'm not that confident about anything before 2000 as the logs don't contain much useful information from that period. Also, I've noticed that several game types that were added after the conversion to KDE, still contained Paul's custom license. I've left Paul's license in those files, but it probably wouldn't be too hard to track contributors done and get permission to change it to just GPL.
    CCMAIL:coolo@kde.org
    
    svn path=/trunk/KDE/kdegames/kpat/; revision=1018358

--- clock.h
+++ clock.h
@@ -1,16 +1,37 @@
 /*
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted,
- * provided that the above copyright notice appear in all copies and that
- * both that copyright notice and this permission notice appear in
- * supporting documentation.
+ * Copyright (C) 2000-2009 Stephan Kulow <coolo@kde.org>
  *
- * This file is provided AS IS with no warranties of any kind.  The author
- * shall have no liability with respect to the infringement of copyrights,
- * trade secrets or any patents by this file or any part thereof.  In no
- * event will the author be liable for any lost revenue or profits or
- * other special, indirect and consequential damages.
-*/
+ * License of original code:
+ * -------------------------------------------------------------------------
+ *   Permission to use, copy, modify, and distribute this software and its
+ *   documentation for any purpose and without fee is hereby granted,
+ *   provided that the above copyright notice appear in all copies and that
+ *   both that copyright notice and this permission notice appear in
+ *   supporting documentation.
+ *
+ *   This file is provided AS IS with no warranties of any kind.  The author
+ *   shall have no liability with respect to the infringement of copyrights,
+ *   trade secrets or any patents by this file or any part thereof.  In no
+ *   event will the author be liable for any lost revenue or profits or
+ *   other special, indirect and consequential damages.
+ * -------------------------------------------------------------------------
+ *
+ * License of modifications/additions made after 2009-01-01:
+ * -------------------------------------------------------------------------
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License as
+ *   published by the Free Software Foundation; either version 2 of 
+ *   the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ */
 
 #ifndef CLOCK_H
 #define CLOCK_H
