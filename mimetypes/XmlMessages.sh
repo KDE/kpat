33e13ba5fa3d028362fcc36142587cbb54ee0439
Burkhard Lück <lueck@hube-lueck.de>
2014-10-30 17:49:19 +0100

    enable kpat mimetypes translations BUG:320983

---
+++ mimetypes/XmlMessages.sh
@@ -0,0 +1,23 @@
+function get_files
+{
+    echo kpatience.xml
+}
+
+function po_for_file
+{
+    case "$1" in
+       kde.xml)
+           echo kpat_mimetypes.po
+       ;;
+    esac
+}
+
+function tags_for_file
+{
+    case "$1" in
+       kpatience.xml)
+           echo comment
+       ;;
+    esac
+}
+
