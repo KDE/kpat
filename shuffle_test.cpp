bf7b4a936b204f85c2c4496dbb1b00c6729e3754
Shlomi Fish <shlomif@shlomifish.org>
2018-10-10 16:16:06 +0300

    Fix the indentation.
    See http://perl-begin.org/tutorials/bad-elements/#no-indentation .

--- shuffle_test.cpp
+++ shuffle_test.cpp
@@ -22,6 +22,7 @@ typedef QList<Card> CardList;
 
 const CardList input({"AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "TC", "TD", "TH", "TS", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS", "KC", "KD", "KH", "KS"}
         );
+
 void TestCardsShuffle::shuffle_seed1()
 {
     CardList have = KpatShuffle::shuffled<Card>(input, 1);
@@ -39,5 +40,6 @@ void TestCardsShuffle::shuffle_seed24()
     );
     QCOMPARE(have, want);
 }
+
 QTEST_MAIN(TestCardsShuffle)
 #include "shuffle_test.moc"
