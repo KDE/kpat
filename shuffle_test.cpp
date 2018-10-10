0062cb25c648db788eb56af55d12c6d5ae20b2c1
Shlomi Fish <shlomif@shlomifish.org>
2018-10-10 16:09:40 +0300

    Rename the test class.

--- shuffle_test.cpp
+++ shuffle_test.cpp
@@ -8,7 +8,7 @@
 #include <QTest>
 #include "shuffle.h"
 
-class TestQString: public QObject
+class TestCardsShuffle: public QObject
 {
     Q_OBJECT
 private slots:
@@ -19,7 +19,7 @@ private slots:
 typedef const char * Card;
 typedef QList<Card> CardList;
 
-void TestQString::shuffle_seed1()
+void TestCardsShuffle::shuffle_seed1()
 {
     CardList in({"AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "TC", "TD", "TH", "TS", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS", "KC", "KD", "KH", "KS"}
         );
@@ -30,7 +30,7 @@ void TestQString::shuffle_seed1()
     QCOMPARE(have, want);
 }
 
-void TestQString::shuffle_seed24()
+void TestCardsShuffle::shuffle_seed24()
 {
     CardList in({"AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "TC", "TD", "TH", "TS", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS", "KC", "KD", "KH", "KS"}
         );
@@ -40,5 +40,5 @@ void TestQString::shuffle_seed24()
     );
     QCOMPARE(have, want);
 }
-QTEST_MAIN(TestQString)
+QTEST_MAIN(TestCardsShuffle)
 #include "shuffle_test.moc"
