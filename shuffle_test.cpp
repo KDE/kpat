b0009617d3fe9e056c8e05f08c91359f148adf27
Shlomi Fish <shlomif@shlomifish.org>
2018-10-10 16:07:55 +0300

    add a test

--- shuffle_test.cpp
+++ shuffle_test.cpp
@@ -13,10 +13,12 @@ class TestQString: public QObject
     Q_OBJECT
 private slots:
     void shuffle_seed1();
+    void shuffle_seed24();
 };
 
 typedef const char * Card;
 typedef QList<Card> CardList;
+
 void TestQString::shuffle_seed1()
 {
     CardList in({"AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "TC", "TD", "TH", "TS", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS", "KC", "KD", "KH", "KS"}
@@ -28,5 +30,15 @@ void TestQString::shuffle_seed1()
     QCOMPARE(have, want);
 }
 
+void TestQString::shuffle_seed24()
+{
+    CardList in({"AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "TC", "TD", "TH", "TS", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS", "KC", "KD", "KH", "KS"}
+        );
+    CardList have = KpatShuffle::shuffled<Card>(in, 24);
+    CardList want(
+        {"AS", "3D", "QD", "2H", "9D", "JD", "7C", "8D", "6D", "KS", "4H", "4S", "8S", "8H", "KC", "TD", "JH", "3S", "3H", "QS", "4D", "AD", "TS", "TC", "5C", "9H", "AC", "8C", "7D", "6S", "KH", "TH", "JC", "6H", "3C", "9C", "6C", "5S", "JS", "KD", "2S", "9S", "QH", "2C", "7S", "AH", "7H", "2D", "5D", "QC", "5H", "4C"}
+    );
+    QCOMPARE(have, want);
+}
 QTEST_MAIN(TestQString)
 #include "shuffle_test.moc"
